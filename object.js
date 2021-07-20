44357ff909ed59b9ec60e3774552f88e1a0c8a14
VarvadiukCursor <dianavarvadiuk@gmail.com>
2021-07-20 22:48:35 +0300

    fix sorting objects

--- object.js
+++ object.js
@@ -30,8 +30,8 @@ const students = [{
     const teaching = Object.keys(student.subjects);
     console.log(teaching);
     let studentSubjects = teaching.map(function(value){
-        let nameSubjects = value.split('_').join('');
-        return nameSubjects.slice(0,1).toUpperCase()+nameSubjects.slice(1);
+        let nameSubject = value.split('_').join(' ');
+        return nameSubject[0].toUpperCase() + nameSubject.slice(1);
     })
     return studentSubjects;
   }
@@ -39,53 +39,55 @@ const students = [{
    //f2
    function getAverageMark(student) {
     const average= Object.values(student.subjects).flat();
-    console.log(average);
-    return Number(average.reduce((sum,element)=>{
-      return sum+=element;
+    return Number(average.reduce((sum, element)=>{
+      return sum += element;
     },0)/average.length).toFixed(2)
       }
       console.log(getAverageMark(students[0]))
     
 //f3
 function getStudentInfo(student) {
-    const {name, course} = student;
+    const {
+      name,
+      course
+    } = student;
     const objectStudentInfo = {
-        course,name,averageMark :getAverageMark(student),
+        course,
+        name,
+        averageMark : getAverageMark(student),
     };
  return objectStudentInfo;
 }
 console.log(getStudentInfo(students[0]));
 //f4
 function getStudentsNames(student) {
- const result = students.map((object, i) => students[i].name).sort();
+ const result = students.map(object => object.name).sort();
 return result;
  }
  console.log(getStudentsNames(students));
 //f5
 function  getBestStudent(students){
-  let bestname='';
-  Object.values(students).forEach(element=>{
+  let bestname = '';
+  Object.values(students).forEach(element=> {
     let bestmark = 0;
     const getmark = getAverageMark(element);
-    if(bestmark < getmark){
+    if(bestmark < getmark)  {
       bestmark = getmark;
       bestname = element.name;
     }
   })
   return bestname;
   }
-  console.log(getBestStudent(students))
-  //f6
-  function calculateWordLetters(letter){
-  const sum = {}
-  if(sum[letter[i]]){
-    sum[letter[i]] += 1;
-  }
-  else{
-    sum[letter[i]] = 1
-  }
-  
-  
-  return sum;
-  }
-  console.log(calculateWordLetters('test'))
+  console.log(getBestStudent(students));
+   //f6
+   function calculateWordLetters(letter){
+  //let newLetter = letter.toLowerCase().split('');
+  let sum = {};
+    for (i = 0; i < letter.length; i++) {
+        if (sum[letter[i]]) {
+          sum[letter[i]] += 1;
+        } else {sum[letter[i]] = 1}
+        }
+    return sum;
+}
+    console.log(calculateWordLetters('test'))
