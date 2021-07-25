57ae312cb2ac94c28318bc4bdcef83062b05889a
VarvadiukCursor <dianavarvadiuk@gmail.com>
2021-07-25 23:25:27 +0300

    chnange some eror objects

--- object.js
+++ object.js
@@ -26,68 +26,79 @@ const students = [{
   }];
   
   //1
-  function getSubjects(student) {
-    const teaching = Object.keys(student.subjects);
-    console.log(teaching);
-    let studentSubjects = teaching.map(function(value){
-        let nameSubject = value.split('_').join(' ');
-        return nameSubject[0].toUpperCase() + nameSubject.slice(1);
-    })
-    return studentSubjects;
-  }
-  console.log(getSubjects(students[0]))
+function getSubjects(student) {
+  const teaching = Object.keys(student.subjects);
+  const resultSubjects = teaching.map(function(value) {
+    const nameSubject = value.split('_').join(' ');
+
+    return nameSubject[0].toUpperCase() + nameSubject.slice(1);
+  })
+
+  return resultSubjects;
+}
+
+  console.log('resultSubjects', getSubjects(students[0]));
+
    //f2
-   function getAverageMark(student) {
-    const average= Object.values(student.subjects).flat();
-    return Number(average.reduce((sum, element)=>{
+function getAverageMark(student) {
+  const average = Object.values(student.subjects).flat();
+
+    return Number(average.reduce((sum, element)=> {
       return sum += element;
     },0)/average.length).toFixed(2)
-      }
-      console.log(getAverageMark(students[0]))
+}
+
+console.log('average', getAverageMark(students[0]))
     
 //f3
 function getStudentInfo(student) {
-    const {
-      name,
-      course
-    } = student;
-    const objectStudentInfo = {
-        course,
-        name,
-        averageMark : getAverageMark(student),
-    };
- return objectStudentInfo;
+  const {name,course} = student;
+  const objectStudentInfo = {
+    course,
+    name,
+    averageMark : getAverageMark(student),
+  };
+
+  return objectStudentInfo;
 }
-console.log(getStudentInfo(students[0]));
+
+console.log('StudentInfo', getStudentInfo(students[0]));
+
 //f4
 function getStudentsNames(student) {
- const result = students.map(object => object.name).sort();
-return result;
- }
- console.log(getStudentsNames(students));
+  const result = students.map(object => object.name).sort();
+
+  return result;
+}
+ console.log('StudentsNames', getStudentsNames(students));
 //f5
 function  getBestStudent(students){
   let bestname = '';
+
   Object.values(students).forEach(element=> {
     let bestmark = 0;
-    const getmark = getAverageMark(element);
-    if(bestmark < getmark)  {
-      bestmark = getmark;
+    const averageMark = getAverageMark(element);
+
+    if (bestmark < averageMark)  {
+      bestmark = averageMark;
       bestname = element.name;
     }
   })
+
   return bestname;
-  }
-  console.log(getBestStudent(students));
-   //f6
-   function calculateWordLetters(letter){
-  //let newLetter = letter.toLowerCase().split('');
-  let sum = {};
-    for (i = 0; i < letter.length; i++) {
-        if (sum[letter[i]]) {
-          sum[letter[i]] += 1;
-        } else {sum[letter[i]] = 1}
-        }
-    return sum;
 }
-    console.log(calculateWordLetters('test'))
+
+console.log('BestStudent', getBestStudent(students));
+
+   //f6
+function calculateWordLetters(letter) {
+  let sum = {};
+  
+  for (i = 0; i < letter.length; i++) {
+    sum[letter[i]] = sum[letter[i]] + 1 || 1;
+  }
+
+  return sum;
+}
+
+console.log('calculateWordLetters', calculateWordLetters('test'));
